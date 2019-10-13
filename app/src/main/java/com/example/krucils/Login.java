f84185502ba702b8af7a4f27f2bbea8d9eb8ab98
raaydr <49266817+raaydr@users.noreply.github.com>
2019-10-13 22:33:13 +0700

    coba

---
+++ app/src/main/java/com/example/krucils/Login.java
@@ -0,0 +1,23 @@
+package com.example.krucils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_user);
+
+
+
+
+
+    }
+}
