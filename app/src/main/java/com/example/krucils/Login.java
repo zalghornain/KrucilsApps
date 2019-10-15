0bf0459e67e3d6201973b3fa98209b18e1a4580a
raaydr <49266817+raaydr@users.noreply.github.com>
2019-10-15 14:52:05 +0700

    test

--- app/src/main/java/com/example/krucils/Login.java
+++ app/src/main/java/com/example/krucils/Login.java
@@ -14,7 +14,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_user);
-
+    //test
 
 
 
