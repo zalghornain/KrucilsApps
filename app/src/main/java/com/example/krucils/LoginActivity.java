501f9f5e139d81e7db72e14ffe11f9f047697833
zalghornain <muh.zall@gmail.com>
2019-10-16 19:06:35 +0700

    login + register

---
+++ app/src/main/java/com/example/krucils/LoginActivity.java
@@ -0,0 +1,95 @@
+package com.example.krucils;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextView UsernameField, PassField, registerBtn;
+    private Button loginBtn;
+    private static final String TAG = "EmailPassword";
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        UsernameField=findViewById(R.id.input_email);
+        PassField=findViewById(R.id.input_password);
+        loginBtn= findViewById(R.id.btn_login);
+        registerBtn=findViewById(R.id.link_signup);
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn(UsernameField.getText().toString(),PassField.getText().toString());
+            }
+
+        });
+
+        //buat register dari dont have an account yet
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        //    if (!validateForm()) {
+        //       return;
+        //   }
+
+        //     showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //         updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //       updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        //    if (!task.isSuccessful()) {
+                        //        mStatusTextView.setText(R.string.auth_failed);
+                    }//   hideProgressDialog();
+                    // [END_EXCLUDE]
+                    // }
+                });
+        // [END sign_in_with_email]
+    }
+}
--- app/src/main/java/com/example/krucils/MainActivity.java
+++ app/src/main/java/com/example/krucils/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.krucils;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,76 +30,49 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity{
     private FirebaseAuth mAuth;
-    private Button loginBtn,adminBtn;
-    private TextView UsernameField, PassField;
-    private static final String TAG = "EmailPassword";
+    private Button registerhomeBtn, loginBtn;
+
+
+    public boolean register = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_user);
+        setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        UsernameField=findViewById(R.id.input_email);
-        PassField=findViewById(R.id.input_password);
+        registerhomeBtn=findViewById(R.id.register);
+        loginBtn=findViewById(R.id.login);
 
-        loginBtn= findViewById(R.id.btn_login);
-
-
-        adminBtn=findViewById(R.id.login_admin);
-    //    FirebaseUser currentUser = mAuth.getCurrentUser();
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn(UsernameField.getText().toString(),PassField.getText().toString());
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(loginIntent);
             }
-
         });
 
 
+        //buat pindah ke register dari home
+        registerhomeBtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+              MainActivity.this.startActivity(registerIntent);
+           }
+        });
+
+
+
     }
 
 
 
-    private void signIn(String email, String password) {
-        Log.d(TAG, "signIn:" + email);
-    //    if (!validateForm()) {
-     //       return;
-     //   }
 
-   //     showProgressDialog();
-
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                   //         updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                     //       updateUI(null);
-                        }
-
-                        // [START_EXCLUDE]
-                    //    if (!task.isSuccessful()) {
-                            //        mStatusTextView.setText(R.string.auth_failed);
-                        }//   hideProgressDialog();
-                        // [END_EXCLUDE]
-                   // }
-                });
-        // [END sign_in_with_email]
-    }
 
 
 
