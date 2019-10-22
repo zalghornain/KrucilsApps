71203b96459cb1115470b6858ff25e6869df3e62
zalghornain <muh.zall@gmail.com>
2019-10-22 20:00:56 +0700

    authentication to profilfragment (sisanya)

--- app/src/main/java/com/example/krucils/LoginActivity.java
+++ app/src/main/java/com/example/krucils/LoginActivity.java
@@ -2,6 +2,10 @@ package com.example.krucils;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -75,8 +79,8 @@ public class LoginActivity extends AppCompatActivity {
                                     Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             //         updateUI(user);
-                            Intent backtomainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                            LoginActivity.this.startActivity(backtomainIntent);
+
+                            //TODO bikin pindah ke fragment yang tadi
                             finish();
                         } else {
                             // If sign in fails, display a message to the user.
--- app/src/main/java/com/example/krucils/ProfilFragment.java
+++ app/src/main/java/com/example/krucils/ProfilFragment.java
@@ -1,17 +1,77 @@
 package com.example.krucils;
 
+import android.app.AlertDialog;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ProfilFragment extends Fragment {
+    private FirebaseAuth mAuth;
+    private Button registerhomeBtn, loginBtn, logoutBtn;
+    private TextView logintitle;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        View v = inflater.inflate(R.layout.activity_main, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerhomeBtn=v.findViewById(R.id.register);
+        loginBtn=v.findViewById(R.id.login);
+        logintitle=v.findViewById(R.id.main_title);
+        logoutBtn=v.findViewById(R.id.logout);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            String name = user.getEmail();
+            logintitle.setText("Selamat datang \n" + name);
+            loginBtn.setVisibility(View.GONE);
+            registerhomeBtn.setVisibility(View.GONE);
+            logoutBtn.setVisibility(View.VISIBLE);
+        }else{
+            String name = "Guest";
+            logintitle.setText("Selamat datang \n" + name);
+            loginBtn.setVisibility(View.VISIBLE);
+            registerhomeBtn.setVisibility(View.VISIBLE);
+            logoutBtn.setVisibility(View.GONE);
+        }
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(loginIntent);
+                //TODO bikin refresh layout setelah login
+            }
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Fragment currentFragment = getActivity().getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+                if (currentFragment instanceof ProfilFragment) {
+                    FragmentTransaction fragTransaction =   (getActivity()).getSupportFragmentManager().beginTransaction();
+                    fragTransaction.detach(currentFragment);
+                    fragTransaction.attach(currentFragment);
+                    fragTransaction.commit();
+                }
+            }
+        });
+
+        return v;
     }
 }
