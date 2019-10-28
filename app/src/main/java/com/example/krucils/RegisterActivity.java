e7ad2132a07409758aaf739fbd6d248cb760a962
zalghornain <muh.zall@gmail.com>
2019-10-28 19:34:45 +0700

    input data login ke database (check duplicate email belum selesai)

--- app/src/main/java/com/example/krucils/RegisterActivity.java
+++ app/src/main/java/com/example/krucils/RegisterActivity.java
@@ -13,17 +13,27 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends MainActivity {
 
 
     private FirebaseAuth mAuth;
     private static final String TAG = "EmailPassword";
-    private TextView userRegister,passRegister;
+    private TextView userRegister,passRegister,emailRegister;
     private Button signupBtn;
+    private FirebaseFirestore db;
+    private static String z;
 
 
 
@@ -32,28 +42,40 @@ public class RegisterActivity extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
         mAuth = FirebaseAuth.getInstance();
-        userRegister= findViewById(R.id.inputregister_email);
+        userRegister= findViewById(R.id.inputregister_username);
+        emailRegister= findViewById(R.id.inputregister_email);
         passRegister=findViewById(R.id.inputregister_password);
         signupBtn=findViewById(R.id.btn_signup);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
 
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                createAccount(userRegister.getText().toString(),passRegister.getText().toString());
-                //todo kalo gak diisi apa2 crash aplikasinya
+                //TODO only register if email is not used
+                //emailIsUsed(mAuth, db, emailRegister.getText().toString());
+                //Log.d(TAG, z);
+                //if(!emailIsUsed(mAuth, db, emailRegister.getText().toString())) {
+                    createAccount(userRegister.getText().toString(), emailRegister.getText().toString(), passRegister.getText().toString());
+                    //todo kalo gak diisi apa2 crash aplikasinya
+                //}
             }
         });
 
+
     }
 
-    private void createAccount(String email, String password) {
+    private void createAccount(String username, String email, String password) {
         Log.d(TAG, "createAccount:" + email);
         // if (!validateForm()) {
         //    return;
         // }
-
         //    showProgressDialog();
+        final String usernamefinal= username;
+        final String emailfinal = email;
+
+
 
         // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -65,11 +87,33 @@ public class RegisterActivity extends MainActivity {
                             Log.d(TAG, "createUserWithEmail:success");
                             Toast.makeText(RegisterActivity.this, "Register success.",
                                     Toast.LENGTH_SHORT).show();
-                            FirebaseUser user = mAuth.getCurrentUser();
+
+
                             //set result buat di cek di profil fragment
+                            //input data to database
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+
+                            //buat nginput sesuatu yang mantap gan
+                            Map<String, Object> data = new HashMap<>();
+
+                            data.put("UID", user.getUid());
+                            data.put("email", user.getEmail());
+                            //todo coba cari cara buat manggil manual jangan di paksa gan
+                            data.put("username", usernamefinal);
+
+                            //apply ke database
+                            //masukin ke dalam document dengan judul UID di koleksi users
+                            db.collection("users").document(user.getUid()).set(data);
+
+
                             setResult(Activity.RESULT_OK);
                             finish();
                             //todo kalo register udah selesai kira kira mau ngapain langkah selanjutnya ?
+
+                            //kalo email duplicate register gagal
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
@@ -84,5 +128,29 @@ public class RegisterActivity extends MainActivity {
                     }
                 });
         // [END create_user_with_email]
+    }
+
+    //ini belum selesai, masih error
+    private boolean emailIsUsed(FirebaseAuth auth, FirebaseFirestore db, String email){
+        FirebaseUser user = auth.getCurrentUser();
+        db.collection("users")
+                .whereEqualTo("email", email)
+                .get()
+                //todo kalo ketemu pertama stop
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task ) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot users : task.getResult()) {
+                                Log.d(TAG, users.getId() + " => " + users.get("email"));
+                                z = users.get("email").toString();
+                                Log.d(TAG, z);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        return true;
     }
 }
