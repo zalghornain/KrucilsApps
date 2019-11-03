ff637843ba69a2d3c4bd28e0a1e45ef48005c161
raaydr <49266817+raaydr@users.noreply.github.com>
2019-11-03 18:02:37 +0700

    Recycler View dan Input ke firestore clear (Basicnya) Paket detail (Basic) dan Input Kelas baru (advanced) Input kelas dalam progres upload image

--- app/src/main/java/com/example/krucils/InputKelasFragment.java
+++ app/src/main/java/com/example/krucils/InputKelasFragment.java
@@ -1,11 +1,20 @@
 package com.example.krucils;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -18,6 +27,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,12 +35,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import static android.app.Activity.RESULT_OK;
+
 public class InputKelasFragment extends Fragment implements View.OnClickListener {
 
     private ProgressBar progressBar;
     private EditText judulKelas, detailKelas, hargaKelas,mulaiKelas;
-    private Button upload;
+    private Button upload, uImage;
     static  final SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+    private ImageView imageView;
+    private Uri filepath;
+    private final int PICK_IMAGE_REQUEST = 71;
     FirebaseFirestore db;
 
 
@@ -47,6 +62,8 @@ public class InputKelasFragment extends Fragment implements View.OnClickListener
         mulaiKelas = v.findViewById(R.id.kelas_mulai);
         upload = v.findViewById(R.id.upload);
         upload.setOnClickListener(this);
+        uImage = v.findViewById(R.id.uImage);
+        uImage.setOnClickListener(this);
 
         db = FirebaseFirestore.getInstance();
 
@@ -56,17 +73,50 @@ public class InputKelasFragment extends Fragment implements View.OnClickListener
     @Override
     public void onClick(View view) {
 
-            String judul = judulKelas.getText().toString();
-            String detail = detailKelas.getText().toString();
-            String harga = hargaKelas.getText().toString();
-            String tanggal = mulaiKelas.getText().toString();
 
-            uploadKelas(judul,detail,harga,tanggal);
-            judulKelas.setText(null);
-            detailKelas.setText(null);
-            hargaKelas.setText(null);
-            mulaiKelas.setText(null);
 
+            if (view.getId()==R.id.upload) {
+                String judul = judulKelas.getText().toString();
+                String detail = detailKelas.getText().toString();
+                String harga = hargaKelas.getText().toString();
+                String tanggal = mulaiKelas.getText().toString();
+                if (isEmpty(judul) && isEmpty(detail) && isEmpty(harga) && isEmpty(tanggal)) {
+                    Toast.makeText(getActivity(), "Tolong dilengkapi kembali", Toast.LENGTH_LONG).show();
+                } else {
+
+                    uploadKelas(judul, detail, harga, tanggal);
+                }
+                judulKelas.setText(null);
+                detailKelas.setText(null);
+                hargaKelas.setText(null);
+                mulaiKelas.setText(null);
+            }
+
+            if (view.getId()==R.id.uImage){
+
+                Intent intent = new Intent();
+                intent.setType("image/");
+                intent.setAction(intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent.createChooser(intent,"Pilih gambar"), PICK_IMAGE_REQUEST);
+
+            }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && requestCode == RESULT_OK && data != null && data.getData()
+        !=null)
+        {
+
+        }
+
+    }
+
+    private boolean isEmpty(String s){
+        return TextUtils.isEmpty(s);
     }
 
     private void uploadKelas(String judul, String detail, String harga, String tanggal) {
