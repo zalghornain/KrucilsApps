96505896d0d062b3388e7f5ba30a31a9496aa1e0
raaydr <49266817+raaydr@users.noreply.github.com>
2019-11-22 13:40:42 +0700

    Detail Kelas

--- app/src/main/java/com/example/krucils/objek/Kelas.java
+++ app/src/main/java/com/example/krucils/objek/Kelas.java
@@ -4,41 +4,37 @@ import java.util.Date;
 
 public class Kelas {
 
-    public int id;
+    public String id;
     public String judul;
-    public int harga;
+    public String hargaFull;
+    public String hargaBiasa;
     public String detail;
     public Date mulaiKelas;
     public boolean check;
     public String imageURL;
+    public Date created;
 
     public Kelas(){
 
     }
 
-    public Kelas(int id, String judul, int harga, String detail, Date mulaiKelas, boolean check, String imageURL) {
+    public Kelas(String id, String judul, String hargaFull, String hargaBiasa, String detail, Date mulaiKelas, boolean check, String imageURL, Date created) {
         this.id = id;
         this.judul = judul;
-        this.harga = harga;
+        this.hargaFull = hargaFull;
+        this.hargaBiasa = hargaBiasa;
         this.detail = detail;
         this.mulaiKelas = mulaiKelas;
         this.check = check;
         this.imageURL = imageURL;
+        this.created = created;
     }
 
-    public boolean isCheck() {
-        return check;
-    }
-
-    public void setCheck(boolean check) {
-        this.check = check;
-    }
-
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -50,12 +46,20 @@ public class Kelas {
         this.judul = judul;
     }
 
-    public int getHarga() {
-        return harga;
+    public String getHargaFull() {
+        return hargaFull;
     }
 
-    public void setHarga(int harga) {
-        this.harga = harga;
+    public void setHargaFull(String hargaFull) {
+        this.hargaFull = hargaFull;
+    }
+
+    public String getHargaBiasa() {
+        return hargaBiasa;
+    }
+
+    public void setHargaBiasa(String hargaBiasa) {
+        this.hargaBiasa = hargaBiasa;
     }
 
     public String getDetail() {
@@ -74,11 +78,27 @@ public class Kelas {
         this.mulaiKelas = mulaiKelas;
     }
 
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
+
     public String getImageURL() {
         return imageURL;
     }
 
     public void setImageURL(String imageURL) {
         this.imageURL = imageURL;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
     }
 }
