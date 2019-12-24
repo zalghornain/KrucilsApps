0d99b767ec7a1e716746deb32ae697a91eeb93ba
raaydr <49266817+raaydr@users.noreply.github.com>
2019-12-24 20:26:11 +0700

    - Perbaikan Logika pada saat input file materi dan Progress Dialognya

--- app/src/main/java/com/example/krucils/Detail_Input_Materi.java
+++ app/src/main/java/com/example/krucils/Detail_Input_Materi.java
@@ -24,6 +24,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -56,6 +57,7 @@ import static android.os.Environment.DIRECTORY_DOWNLOADS;
 public class Detail_Input_Materi extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     FirebaseFirestore db;
     private Spinner spinner;
+    private ProgressBar progressBar;
     private FirebaseAuth mAuth;
     private ImageView image;
     private TextView judul,detail,kelasMulai,harga,detail_harga;
@@ -97,6 +99,9 @@ public class Detail_Input_Materi extends AppCompatActivity implements View.OnCli
         checkKelas=findViewById(R.id.btn_check);
         materiFile=findViewById(R.id.checkbox_file);
         materiLink=findViewById(R.id.uncheckbox_file);
+
+        progressBar = findViewById(R.id.progressKelas);
+        progressBar.setVisibility(View.GONE);
 
         materiLink.setOnClickListener(this);
         materiFile.setOnClickListener(this);
@@ -211,11 +216,11 @@ public class Detail_Input_Materi extends AppCompatActivity implements View.OnCli
                 String judul = judulMateri.getText().toString();
                 String urlLink = linkMateri.getText().toString();
 
-                if (!judul.isEmpty() && file != null ) {
+                if (!judul.isEmpty() && file != null && urlLink.isEmpty() ) {
 
 
                     if (uploadTask != null && uploadTask.isInProgress()) {
-                        Toast.makeText(getApplication(), "sedang diproses", Toast.LENGTH_LONG).show();
+                        Toast.makeText(Detail_Input_Materi.this , "sedang diproses", Toast.LENGTH_LONG).show();
 
 
                     } else {
@@ -234,6 +239,10 @@ public class Detail_Input_Materi extends AppCompatActivity implements View.OnCli
                                                 final Uri downloadUrl = uri;
                                                 String urlfile = downloadUrl.toString();
                                                 uploadMateri(judul, urlfile, UIDuser, username, uidAkses,typeFile);
+                                                progressDialog.dismiss();
+                                                judulMateri.setText(null);
+                                                file = null;
+                                                linkMateri.setText(null);
 
                                             }
                                         });
@@ -261,28 +270,27 @@ public class Detail_Input_Materi extends AppCompatActivity implements View.OnCli
                     }
 
 
+
+
                 }
-                    if(!judul.isEmpty() && file == null  ){
+               else if(!judul.isEmpty() && file == null && !urlLink.isEmpty() ){
 
-
-                        uploadMateri(judul, urlLink, UIDuser, username, uidAkses,typeFile);
-
-                    } if (judul.isEmpty() && file == null && !urlLink.isEmpty()){
-
-                progressDialog.dismiss();
-                Toast.makeText(Detail_Input_Materi.this, "Tolong dilengkapi kembali", Toast.LENGTH_LONG).show();
-            }
-              /* if(judul.isEmpty() && urlLink.isEmpty() ) {
                     progressDialog.dismiss();
-                    Toast.makeText(Detail_Input_Materi.this, "Tolong dilengkapi kembali", Toast.LENGTH_LONG).show();
+                    uploadMateri(judul, urlLink, UIDuser, username, uidAkses,typeFile);
+                    judulMateri.setText(null);
+                    file = null;
+                    linkMateri.setText(null);
+
+                    }
+                else {
+                    progressDialog.dismiss();
+                    Toast.makeText(Detail_Input_Materi.this, "Tolong disi", Toast.LENGTH_LONG).show();
                 }
 
-               */
-                judulMateri.setText(null);
-                file = null;
-                linkMateri.setText(null);
 
-                progressDialog.dismiss();
+
+
+
 
                 break;
             case R.id.btn_upload:
