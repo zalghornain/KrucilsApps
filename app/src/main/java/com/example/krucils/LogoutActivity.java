69d72c1696d5590ab042a303ccfe31085950622f
zalghornain <muh.zall@gmail.com>
2019-12-31 17:02:28 +0700

    tambah google api logout biar bisa choose account pas login lagi

--- app/src/main/java/com/example/krucils/LogoutActivity.java
+++ app/src/main/java/com/example/krucils/LogoutActivity.java
@@ -9,26 +9,37 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LogoutActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private TextView name;
     private Button btn_ya,btn_tidak;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logout);
         mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         btn_ya = findViewById(R.id.btn_ya);
         btn_ya.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mGoogleSignInClient.signOut();
                 mAuth.signOut();
-                Intent logout = new Intent(LogoutActivity.this, Beranda.class);
-                startActivity(logout);
                 finish();
             }
         });
